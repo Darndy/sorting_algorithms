2ffe69ddfb3b08f78e93669ae2f6b0f3b6ca573d
darndy <godonudaniel@gmail.com>
2023-12-18 12:02:58 -0800

    sorting and algorithm

---
+++ sort.h
@@ -0,0 +1,57 @@
+#ifndef _SORT_H_
+#define _SORT_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void print_array(const int *array, size_t size);
+
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+
+typedef struct array_s
+{
+	int *bg;
+	int mid;
+	int size;
+} array_t;
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+listint_t *rejoin(listint_t *list, listint_t *sorted);
+listint_t *sorted_insert(listint_t *temp, listint_t **head);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+int partition(int *array, int start, int end, size_t size);
+void sorter(int *array, int start, int end, size_t size);
+void swap(listint_t **store, listint_t **ptr, listint_t **list);
+void swap_prev(listint_t **store, listint_t **temp, listint_t **list);
+void shell_sort(int *array, size_t size);
+size_t knuth_seq(size_t size);
+void swap_int(int *a, int *b);
+void cocktail_sort_list(listint_t **list);
+void swap_node(listint_t **_nodea, listint_t **_nodeb,
+		listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+array_t sort_helper(array_t sup_arr, int *space);
+array_t merge(array_t sup_arr, array_t left, array_t right,
+                int *space);
+array_t create_array(int *array, int size, int start);
+void print_merge(array_t left, array_t right);
+
+
+#endif /*_SORT_H_*/
