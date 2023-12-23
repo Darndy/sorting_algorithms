a491a503d216ce7a4d31acfaff2c73a146e9c255
Essy <esthercikechukwu@gmail.com>
2023-12-23 13:47:26 +0000

    bubble

--- sort.h
+++ sort.h
@@ -4,8 +4,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define UP 0
+#define DOWN 1
 
 void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 
 typedef struct listint_s
 {
@@ -14,6 +17,13 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
 
 typedef struct array_s
 {
@@ -23,10 +33,11 @@ typedef struct array_s
 } array_t;
 
 /**
- *  * enum bool - Enumeration of Boolean values.
- *   * @false: Equals 0.
- *    * @true: Equals 1.
- *     */
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+
 typedef enum bool
 {
 		false = 0,
