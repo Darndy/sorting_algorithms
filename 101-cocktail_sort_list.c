2a4ed14b6595867e505f8b4cc2f700e978c767ee
Essy <esthercikechukwu@gmail.com>
2023-12-23 14:10:11 +0000

    cocktail

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -69,45 +69,57 @@ void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker)
  * algorithm and prints out the list after each swap
  * @list: list to sort
  */
+
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *list1, *list2, *temp/*, *store, *store2*/;
-	int swapped;
+    listint_t *list2, *temp;
+    int swapped;
 
-	if (!list || !(*list) || !(*list)->next)
-		return;
-	list1 = *list;
-	list2 = *list;
-	/*store = NULL;*/
-	temp = *list;
-	for (; list1; list1 = list1->next)
-	{
-		list2 = temp;
-		swapped = 0;
-		while (list2)
-		{
-			temp = list2;
-			list2 = list2->next;
-			if (!list2)
-				break;
-			if (temp->n > list2->n)
-			{
-				swap_node(&temp, &list2, list);
-				swapped = 1;
-			} } /*store = */list2 = temp;
-			print_list(*list);
-		while (list2)
-		{
-			temp = list2;
-			list2 = list2->prev;
-			if (!list2)
-				break;
-			if (temp->n < list2->n)
-			{
-				swap_node(&list2, &temp, list);
-				swapped = 1;
-			} } /*store2 = temp;*/
-			print_list(*list);
-		if (!swapped)
-			break;
-	} }
+    if (!list || !(*list) || !(*list)->next)
+        return;
+
+    temp = *list;
+
+    for (; temp; temp = temp->next)
+    {
+        list2 = temp;
+        swapped = 0;
+
+        while (list2)
+        {
+            temp = list2;
+            list2 = list2->next;
+
+            if (!list2)
+                break;
+
+            if (temp->n > list2->n)
+            {
+                swap_node(&temp, &list2, list);
+                swapped = 1;
+            }
+        }
+
+        list2 = temp;
+
+        while (list2)
+        {
+            temp = list2;
+            list2 = list2->prev;
+
+            if (!list2)
+                break;
+
+            if (temp->n < list2->n)
+            {
+                swap_node(&list2, &temp, list);
+                swapped = 1;
+            }
+        }
+
+        print_list(*list);
+
+        if (!swapped)
+            break;
+    }
+}
