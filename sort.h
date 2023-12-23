7b8931e20299bc08261e04bf53ce14313045ec31
Essy <esthercikechukwu@gmail.com>
2023-12-23 13:54:51 +0000

    bubble

--- sort.h
+++ sort.h
@@ -4,11 +4,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define UP 0
-#define DOWN 1
-
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
+/**
+ * struct listint_s - Doubly linked list node
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
 
 typedef struct listint_s
 {
@@ -17,13 +18,6 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
- */
 
 typedef struct array_s
 {
